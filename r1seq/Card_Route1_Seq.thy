6eff89be4e4861d9943c0574064d72d9b2ed8a68
asplos272 <asplos272@gmail.com>
2026-06-20 23:04:20 +0800

    seq: prove RCHAR single root card helper

--- r1seq/Card_Route1_Seq.thy
+++ r1seq/Card_Route1_Seq.thy
@@ -4,9 +4,18 @@ begin
 
 (* ===================================================================== *)
 (* LANE SEQ-HEAD — prove seq_head_core_le_rsize.  See ROUTE_SEQ.md.       *)
-(* Build: scripts\codex-isabelle-build-posix.ps1 -Session Posix_Card_Route1_Seq *)
+(* Build with the private USER_HOME command from ROUTE_SEQ.md.             *)
 (* NO sorry. Build green. Fail-stop + report.                            *)
 (* ===================================================================== *)
+
+abbreviation A :: "rrexp \<Rightarrow> rrexp \<Rightarrow> rrexp set" where
+  "A r k \<equiv> strong_apder_acc r k"
+
+abbreviation B :: "rrexp \<Rightarrow> rrexp set" where
+  "B k \<equiv> A RONE k"
+
+definition D1 :: "rrexp \<Rightarrow> rrexp \<Rightarrow> nat" where
+  "D1 q k = card (A (RALTS [q]) k - B k)"
 
 definition single_root :: "rrexp \<Rightarrow> rrexp \<Rightarrow> rrexp set" where
   "single_root q k =
@@ -14,6 +23,104 @@ definition single_root :: "rrexp \<Rightarrow> rrexp \<Rightarrow> rrexp set" wh
 definition single_term :: "rrexp \<Rightarrow> rrexp \<Rightarrow> rrexp set" where
   "single_term q k =
      rsimpStrong_dlform_closure (apder_term_frontier_acc q k)"
+
+lemma finite_single_root [simp]: "finite (single_root q k)"
+  by (simp add: single_root_def)
+
+lemma finite_single_term [simp]: "finite (single_term q k)"
+  by (simp add: single_term_def)
+
+lemma A_single_decomp:
+  "A (RALTS [q]) k = single_root q k \<union> single_term q k"
+  by (simp add: strong_apder_acc_def single_root_def single_term_def
+      rsimpStrong_dlform_closure_def)
+
+lemma B_alt:
+  "B k = rsimpStrong_dlform_closure (rfrontier k)"
+  by (simp add: strong_apder_acc_def rsimpStrong_dlform_closure_def)
+
+lemma apder_nf_s4:
+  assumes "apder_nf r" "apder_nf k"
+  shows "apder_nf (rsimp4_SEQ_atom r k)"
+  using assms
+proof (induction r arbitrary: k)
+  case RZERO
+  then show ?case by simp
+next
+  case RONE
+  then show ?case by simp
+next
+  case (RCHAR c)
+  then show ?case by (cases k) auto
+next
+  case (RSEQ r1 r2)
+  then show ?case by simp
+next
+  case (RALTS rs)
+  then show ?case by (cases k) auto
+next
+  case (RSTAR r)
+  then show ?case by (cases k) auto
+next
+  case (RNTIMES r n)
+  then show ?case by (cases k) auto
+next
+  case (RBACKREF4 r1 r2 r3 r4 cs)
+  then show ?case by (cases k) auto
+next
+  case (RHALF r cs rep)
+  then show ?case by (cases k) auto
+next
+  case (RRESIDUE cs rep)
+  then show ?case by (cases k) auto
+qed
+
+lemma single_term_RZERO [simp]: "single_term RZERO k = {}"
+  by (simp add: single_term_def rsimpStrong_dlform_closure_def)
+
+lemma single_term_RONE [simp]: "single_term RONE k = {}"
+  by (simp add: single_term_def rsimpStrong_dlform_closure_def)
+
+lemma single_term_RCHAR [simp]: "single_term (RCHAR c) k = B k"
+  by (simp add: single_term_def B_alt)
+
+lemma single_term_RSEQ [simp]:
+  "single_term (RSEQ r1 r2) k =
+    single_term r1 (rsimp4_SEQ_atom r2 k) \<union> single_term r2 k"
+  by (simp add: single_term_def rsimpStrong_dlform_closure_def)
+
+lemma card_row_dlforms_rsimp4_SEQ_atom_RCHAR_le_one [simp]:
+  "card (row_dlforms (rsimp4_SEQ_atom (RCHAR c) k)) \<le> 1"
+  by (cases k) auto
+
+lemma card_row_dlforms_RSEQ_RCHAR_le_one [simp]:
+  "card (row_dlforms (RSEQ (RCHAR c) k)) \<le> 1"
+  by simp
+
+lemma card_row_dlforms_rsimp4_SEQ_atom_RCHAR_s4_s4_le_one [simp]:
+  "card (row_dlforms
+      (rsimp4_SEQ_atom (RCHAR c) (rsimp4_SEQ_atom r (rsimp4_SEQ_atom s k)))) \<le> 1"
+  by (rule card_row_dlforms_rsimp4_SEQ_atom_RCHAR_le_one)
+
+lemma card_row_dlforms_rsimp4_SEQ_atom_RCHAR_s4_s4_le_Suc0:
+  "card (row_dlforms
+      (rsimp4_SEQ_atom (RCHAR c) (rsimp4_SEQ_atom r (rsimp4_SEQ_atom s k)))) \<le> Suc 0"
+proof -
+  have "card (row_dlforms
+      (rsimp4_SEQ_atom (RCHAR c) (rsimp4_SEQ_atom r (rsimp4_SEQ_atom s k)))) \<le> 1"
+    by (rule card_row_dlforms_rsimp4_SEQ_atom_RCHAR_s4_s4_le_one)
+  then show ?thesis
+    by simp
+qed
+
+lemma single_root_RSEQ_RCHAR_card_le_one:
+  "card (single_root (RSEQ (RCHAR c) t) k) \<le> 1"
+  unfolding single_root_def
+  apply (cases k; cases "rsimpStrong_raw t"; cases "rsimpStrong_raw k")
+  apply (auto simp add: rsimpStrong_dlform_closure_def rsimp7_SEQ_atom_def
+      rsimpStrong_ALTs_raw_def rsimpStrong_prune_rows_raw_def
+      rsimpStrong_prune_pair_raw_def Let_def)
+  by (metis card_row_dlforms_rsimp4_SEQ_atom_RCHAR_s4_s4_le_Suc0)+
 
 (* TARGET (prove below; statement + steer in ROUTE_SEQ.md):
    lemma seq_head_core_le_rsize:
