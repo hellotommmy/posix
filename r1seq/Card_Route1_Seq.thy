2a7513eed4d14e181129507f1110f8f77dd46270
asplos272 <asplos272@gmail.com>
2026-06-22 02:15:58 +0800

    seq: add concrete RSEQ char-star root-shift

--- r1seq/Card_Route1_Seq.thy
+++ r1seq/Card_Route1_Seq.thy
@@ -866,6 +866,15 @@ lemma single_root_RSEQ_RSTAR_root_shift_RCHAR_RSEQ_RCHAR_RCHAR_RCHAR:
     rsimp7_SEQ_atom_def rsimpStrong_ALTs_raw_def rsimpStrong_prune_rows_raw_def
     split: if_splits intro!: card_subset_singleton_le_one)
 
+lemma single_root_RSEQ_RSTAR_root_shift_RCHAR_RSEQ_RCHAR_RSTAR_RCHAR:
+  "card (single_root (RSEQ (RSTAR (RCHAR d)) (RSEQ (RCHAR c) (RSTAR e))) (RCHAR a) -
+      B (rsimp4_SEQ_atom (RSTAR (RCHAR d))
+        (rsimp4_SEQ_atom (RSEQ (RCHAR c) (RSTAR e)) (RCHAR a)))) \<le> 1"
+  by (cases "rsimpStrong_raw e")
+    (auto simp add: single_root_def B_alt rsimpStrong_dlform_closure_def
+      rsimp7_SEQ_atom_def rsimpStrong_ALTs_raw_def rsimpStrong_prune_rows_raw_def
+      split: if_splits intro!: card_subset_singleton_le_one)
+
 lemma seq_head_core_RSTAR_le_Suc_rsize_from_root_shift_child:
   fixes r t k
   defines "c \<equiv> rsimp4_SEQ_atom t k"
