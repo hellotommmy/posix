1a88ea2b8ca6d8648df208d75ac6d45889a622f9
asplos272 <asplos272@gmail.com>
2026-06-22 02:19:27 +0800

    seq: add singleton RALTS one-branch root-shift

--- r1seq/Card_Route1_Seq.thy
+++ r1seq/Card_Route1_Seq.thy
@@ -892,6 +892,15 @@ lemma single_root_RSEQ_RSTAR_root_shift_RCHAR_RALTS_single_RCHAR:
       rsimp7_SEQ_atom_def rsimpStrong_ALTs_raw_def rsimpStrong_prune_rows_raw_def
       split: if_splits intro!: card_subset_singleton_le_one)
 
+lemma single_root_RSEQ_RSTAR_root_shift_RCHAR_RALTS_single_RONE:
+  "card (single_root (RSEQ (RSTAR r) (RALTS [RONE])) (RCHAR a) -
+      B (rsimp4_SEQ_atom (RSTAR r)
+        (rsimp4_SEQ_atom (RALTS [RONE]) (RCHAR a)))) \<le> 1"
+  by (cases "rsimpStrong_raw r")
+    (auto simp add: single_root_def B_alt rsimpStrong_dlform_closure_def
+      rsimp7_SEQ_atom_def rsimpStrong_ALTs_raw_def rsimpStrong_prune_rows_raw_def
+      split: if_splits intro!: card_subset_singleton_le_one)
+
 lemma single_root_RSEQ_RSTAR_root_shift_RCHAR_RALTS_single_RSTAR:
   "card (single_root (RSEQ (RSTAR r) (RALTS [RSTAR s])) (RCHAR a) -
       B (rsimp4_SEQ_atom (RSTAR r)
