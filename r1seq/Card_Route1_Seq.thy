7c9d49ffa2b2211cbec45c50f1265c5c1f0fe59d
asplos272 <asplos272@gmail.com>
2026-06-22 00:41:03 +0800

    seq: add D1 induction from combined RSEQ budget

--- r1seq/Card_Route1_Seq.thy
+++ r1seq/Card_Route1_Seq.thy
@@ -1076,6 +1076,77 @@ proof -
         [OF L1 seq_head boundary clean])
 qed
 
+lemma D1_singleton_le_rsize_from_combined_RSEQ_L1_clean:
+  assumes L1:
+    "\<And>rs k. A (RALTS [RALTS rs]) k \<subseteq>
+      (\<Union>q \<in> set rs. A (RALTS [q]) k)"
+    and rseq_combined:
+    "\<And>r1 r2 k. legacy_rrexp (RSEQ r1 r2) \<Longrightarrow>
+      rntimes_free (RSEQ r1 r2) \<Longrightarrow> apder_nf (RSEQ r1 r2) \<Longrightarrow>
+      apder_nf k \<Longrightarrow>
+      card ((single_root (RSEQ r1 r2) k \<union>
+          single_term r1 (rsimp4_SEQ_atom r2 k)) -
+          (B k \<union> B (rsimp4_SEQ_atom r2 k))) +
+        card ((B (rsimp4_SEQ_atom r2 k) - B k) \<union>
+          (single_term r2 k - B k))
+        \<le> rsize (RSEQ r1 r2)"
+    and clean: "legacy_rrexp q" "rntimes_free q" "apder_nf q" "apder_nf k"
+  shows "D1 q k \<le> rsize q"
+  using clean
+proof (induction q arbitrary: k)
+  case RZERO
+  then show ?case
+    using D1_RZERO_le_rsize[of k] by simp
+next
+  case RONE
+  then show ?case
+    using D1_RONE_le_rsize[of k] by simp
+next
+  case (RCHAR c)
+  then show ?case
+    using D1_RCHAR_le_rsize[of k c] by simp
+next
+  case (RSEQ q1 q2)
+  have combined:
+    "card ((single_root (RSEQ q1 q2) k \<union>
+        single_term q1 (rsimp4_SEQ_atom q2 k)) -
+        (B k \<union> B (rsimp4_SEQ_atom q2 k))) +
+      card ((B (rsimp4_SEQ_atom q2 k) - B k) \<union>
+        (single_term q2 k - B k))
+      \<le> rsize (RSEQ q1 q2)"
+    using RSEQ.prems by (intro rseq_combined) auto
+  show ?case
+    by (rule D1_RSEQ_le_rsize_from_combined_head_boundary)
+      (use combined in simp)
+next
+  case (RALTS rs)
+  have branch: "\<And>q. q \<in> set rs \<Longrightarrow> D1 q k \<le> rsize q"
+    using RALTS.IH RALTS.prems by auto
+  show ?case
+    by (rule D1_RALTS_le_rsize_from_L1_and_branches[OF L1 branch])
+next
+  case (RSTAR q)
+  let ?c = "rsimp4_SEQ_atom (RSTAR q) k"
+  have nf_c: "apder_nf ?c"
+    using RSTAR.prems by (intro apder_nf_s4) auto
+  have child: "D1 q ?c \<le> rsize q"
+    using RSTAR.IH[OF _ _ _ nf_c] RSTAR.prems by auto
+  show ?case
+    using D1_RSTAR_le_rsize_from_child[of q k] RSTAR.prems child by auto
+next
+  case (RNTIMES q n)
+  then show ?case by simp
+next
+  case (RBACKREF4 q1 q2 q3 q4 cs)
+  then show ?case by simp
+next
+  case (RHALF q cs rep)
+  then show ?case by simp
+next
+  case (RRESIDUE cs rep)
+  then show ?case by simp
+qed
+
 (* TARGET (prove below; statement + steer in ROUTE_SEQ.md):
    lemma seq_head_core_le_rsize:
      assumes "apder_nf h" "apder_nf t" "apder_nf k"
