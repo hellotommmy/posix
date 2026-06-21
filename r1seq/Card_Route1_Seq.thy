06d6982bcf452f4f72e64f46eebede0e32bf3d29
asplos272 <asplos272@gmail.com>
2026-06-22 03:13:50 +0800

    seq: add exact RSTAR one-continuation core

--- r1seq/Card_Route1_Seq.thy
+++ r1seq/Card_Route1_Seq.thy
@@ -2012,6 +2012,46 @@ proof -
         [OF clean(1) clean(2) _ root_shift child]) simp
 qed
 
+lemma seq_head_core_RSTAR_le_rsize_RONE_from_tail_cases_clean:
+  assumes rseq_tail:
+    "\<And>t1 t2. legacy_rrexp (RSEQ t1 t2) \<Longrightarrow>
+      rntimes_free (RSEQ t1 t2) \<Longrightarrow> apder_nf (RSEQ t1 t2) \<Longrightarrow>
+      card ((single_root (RSEQ (RSTAR r) (RSEQ t1 t2)) RONE -
+        B (rsimp4_SEQ_atom (RSTAR r)
+          (rsimp4_SEQ_atom (RSEQ t1 t2) RONE))) \<union>
+        (B (rsimp4_SEQ_atom (RSTAR r)
+          (rsimp4_SEQ_atom (RSEQ t1 t2) RONE)) -
+          (B RONE \<union> B (rsimp4_SEQ_atom (RSEQ t1 t2) RONE)))) \<le> 1"
+    and ralts_tail:
+    "\<And>rs. legacy_rrexp (RALTS rs) \<Longrightarrow>
+      rntimes_free (RALTS rs) \<Longrightarrow> apder_nf (RALTS rs) \<Longrightarrow>
+      card ((single_root (RSEQ (RSTAR r) (RALTS rs)) RONE -
+        B (rsimp4_SEQ_atom (RSTAR r)
+          (rsimp4_SEQ_atom (RALTS rs) RONE))) \<union>
+        (B (rsimp4_SEQ_atom (RSTAR r)
+          (rsimp4_SEQ_atom (RALTS rs) RONE)) -
+          (B RONE \<union> B (rsimp4_SEQ_atom (RALTS rs) RONE)))) \<le> 1"
+    and child:
+    "D1 r (rsimp4_SEQ_atom (RSTAR r) (rsimp4_SEQ_atom t RONE)) \<le> rsize r"
+    and clean: "legacy_rrexp t" "rntimes_free t" "apder_nf t"
+  shows
+    "card ((single_root (RSEQ (RSTAR r) t) RONE \<union>
+        single_term (RSTAR r) (rsimp4_SEQ_atom t RONE)) -
+        (B RONE \<union> B (rsimp4_SEQ_atom t RONE))) \<le>
+      rsize (RSTAR r)"
+proof -
+  have root_boundary:
+    "card ((single_root (RSEQ (RSTAR r) t) RONE -
+      B (rsimp4_SEQ_atom (RSTAR r) (rsimp4_SEQ_atom t RONE))) \<union>
+      (B (rsimp4_SEQ_atom (RSTAR r) (rsimp4_SEQ_atom t RONE)) -
+        (B RONE \<union> B (rsimp4_SEQ_atom t RONE)))) \<le> 1"
+    by (rule single_root_RSEQ_RSTAR_root_boundary_RONE_from_tail_cases_clean
+        [OF rseq_tail ralts_tail clean])
+  show ?thesis
+    by (rule seq_head_core_RSTAR_le_rsize_from_root_boundary_child
+        [OF root_boundary child])
+qed
+
 lemma D1_RSTAR_le_rsize_from_child:
   assumes "apder_nf r" "apder_nf k"
     and "D1 r (rsimp4_SEQ_atom (RSTAR r) k) \<le> rsize r"
