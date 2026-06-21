7998ac65bf9ffa84805298f8ac41881d5a299762
asplos272 <asplos272@gmail.com>
2026-06-22 03:10:49 +0800

    seq: add exact RSTAR one-star shift

--- r1seq/Card_Route1_Seq.thy
+++ r1seq/Card_Route1_Seq.thy
@@ -764,6 +764,16 @@ lemma single_root_RSEQ_RSTAR_root_boundary_RONE_RCHAR:
       rsimp7_SEQ_atom_def rsimpStrong_ALTs_raw_def rsimpStrong_prune_rows_raw_def
       split: if_splits intro!: card_subset_singleton_le_one)
 
+lemma single_root_RSEQ_RSTAR_root_boundary_RONE_RSTAR:
+  "card ((single_root (RSEQ (RSTAR r) (RSTAR s)) RONE -
+      B (rsimp4_SEQ_atom (RSTAR r) (rsimp4_SEQ_atom (RSTAR s) RONE))) \<union>
+      (B (rsimp4_SEQ_atom (RSTAR r) (rsimp4_SEQ_atom (RSTAR s) RONE)) -
+        (B RONE \<union> B (rsimp4_SEQ_atom (RSTAR s) RONE)))) \<le> 1"
+  by (cases "rsimpStrong_raw r"; cases "rsimpStrong_raw s")
+    (auto simp add: single_root_def B_alt rsimpStrong_dlform_closure_def
+      rsimp7_SEQ_atom_def rsimpStrong_ALTs_raw_def rsimpStrong_prune_rows_raw_def
+      split: if_splits intro!: card_subset_singleton_le_one)
+
 lemma single_root_RSEQ_RSTAR_root_shift_RCHAR_RZERO [simp]:
   "card (single_root (RSEQ (RSTAR r) RZERO) (RCHAR a) -
       B (rsimp4_SEQ_atom (RSTAR r) (rsimp4_SEQ_atom RZERO (RCHAR a)))) \<le> 1"
