5605c54bcecf52cb83552a790c47952b8b513911
asplos272 <asplos272@gmail.com>
2026-06-22 01:04:02 +0800

    Add star escape credit row helpers

--- r1cover/Card_Route1_Cover.thy
+++ r1cover/Card_Route1_Cover.thy
@@ -753,6 +753,28 @@ next
     using star_fix by (simp add: rsimp7_SEQ_atom_def)
 qed (use star_fix in \<open>simp_all add: rsimp7_SEQ_atom_def\<close>)
 
+lemma row_dlforms_rsimp_ALTs_RONE_star_tail_credit_fix:
+  assumes alt_one: "rsimp_ALTs ps = RONE"
+    and star_fix: "rsimpStrong_raw (RSTAR s) = RSTAR s"
+  shows "row_dlforms
+      (rsimp7_SEQ_atom
+        (rsimp7_SEQ_atom (rsimp_ALTs ps) (RSTAR s))
+        (rsimpStrong_raw k)) \<subseteq>
+    row_dlforms (rsimpStrong_raw (rsimp4_SEQ_atom (RSTAR s) k))"
+  using alt_one row_dlforms_RSTAR_tail_credit_fix[OF star_fix, of k]
+  by simp
+
+lemma row_dlforms_rsimp_ALTs_RSTAR_self_credit_fix:
+  assumes alt_star: "rsimp_ALTs ps = RSTAR s"
+    and star_fix: "rsimpStrong_raw (RSTAR s) = RSTAR s"
+  shows "row_dlforms
+      (rsimp7_SEQ_atom
+        (rsimp7_SEQ_atom (rsimp_ALTs ps) (RSTAR s))
+        (rsimpStrong_raw k)) \<subseteq>
+    row_dlforms (rsimpStrong_raw (rsimp4_SEQ_atom (RSTAR s) k))"
+  using alt_star row_dlforms_RSTAR_tail_credit_fix[OF star_fix, of k]
+  by (simp add: rsimp7_SEQ_atom_def)
+
 type_synonym tagged_row = "rrexp \<times> rrexp"
 
 lemma map_snd_map_Pair [simp]:
