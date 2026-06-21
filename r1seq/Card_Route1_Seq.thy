36d219fd2bcf7d551b9a5c2ed13c556a7bc92dd5
asplos272 <asplos272@gmail.com>
2026-06-22 00:51:53 +0800

    seq: add suc-head D1 assembly

--- r1seq/Card_Route1_Seq.thy
+++ r1seq/Card_Route1_Seq.thy
@@ -581,6 +581,60 @@ proof -
     using child by simp
 qed
 
+lemma D1_RSEQ_step_from_head_boundary_budgets:
+  fixes r1 r2 k
+  defines "c \<equiv> rsimp4_SEQ_atom r2 k"
+  assumes seq_head:
+    "card ((single_root (RSEQ r1 r2) k \<union> single_term r1 c) -
+      (B k \<union> B c)) \<le> H"
+    and boundary:
+    "card ((B c - B k) \<union> (single_term r2 k - B k)) \<le> K"
+  shows "D1 (RSEQ r1 r2) k \<le> H + K"
+proof -
+  let ?Root = "single_root (RSEQ r1 r2) k"
+  let ?T1 = "single_term r1 c"
+  let ?T2 = "single_term r2 k"
+  have decomp:
+    "A (RALTS [RSEQ r1 r2]) k = ?Root \<union> ?T1 \<union> ?T2"
+    by (auto simp add: A_single_decomp c_def)
+  have incl:
+    "A (RALTS [RSEQ r1 r2]) k - B k \<subseteq>
+      ((?Root \<union> ?T1) - (B k \<union> B c)) \<union>
+      ((B c - B k) \<union> (?T2 - B k))"
+    using decomp by auto
+  have "D1 (RSEQ r1 r2) k \<le>
+      card (((?Root \<union> ?T1) - (B k \<union> B c)) \<union>
+        ((B c - B k) \<union> (?T2 - B k)))"
+    unfolding D1_def
+    by (rule card_mono) (use incl in auto)
+  also have "... \<le>
+      card ((?Root \<union> ?T1) - (B k \<union> B c)) +
+      card ((B c - B k) \<union> (?T2 - B k))"
+    by (rule card_Un_le)
+  also have "... \<le> H + K"
+    using seq_head boundary by simp
+  finally show ?thesis .
+qed
+
+lemma D1_RSEQ_le_rsize_from_head_suc_boundary_child:
+  fixes r1 r2 k
+  assumes seq_head:
+    "card ((single_root (RSEQ r1 r2) k \<union>
+      single_term r1 (rsimp4_SEQ_atom r2 k)) -
+      (B k \<union> B (rsimp4_SEQ_atom r2 k))) \<le> Suc (rsize r1)"
+    and boundary:
+    "card ((B (rsimp4_SEQ_atom r2 k) - B k) \<union>
+      (single_term r2 k - B k)) \<le> D1 r2 k"
+    and child: "D1 r2 k \<le> rsize r2"
+  shows "D1 (RSEQ r1 r2) k \<le> rsize (RSEQ r1 r2)"
+proof -
+  have "D1 (RSEQ r1 r2) k \<le> Suc (rsize r1) + D1 r2 k"
+    by (rule D1_RSEQ_step_from_head_boundary_budgets)
+      (use seq_head boundary in simp_all)
+  then show ?thesis
+    using child by simp
+qed
+
 lemma D1_RSEQ_le_rsize_from_combined_head_boundary:
   fixes r1 r2 k
   defines "c \<equiv> rsimp4_SEQ_atom r2 k"
@@ -922,6 +976,80 @@ next
     using RSEQ.IH(2) RSEQ.prems by auto
   show ?case
     by (rule D1_RSEQ_le_rsize_from_seq_head_boundary_child[OF sh bnd child])
+next
+  case (RALTS rs)
+  have branch: "\<And>q. q \<in> set rs \<Longrightarrow> D1 q k \<le> rsize q"
+    using RALTS.IH RALTS.prems by auto
+  show ?case
+    by (rule D1_RALTS_le_rsize_from_L1_and_branches[OF L1 branch])
+next
+  case (RSTAR q)
+  let ?c = "rsimp4_SEQ_atom (RSTAR q) k"
+  have nf_c: "apder_nf ?c"
+    using RSTAR.prems by (intro apder_nf_s4) auto
+  have child: "D1 q ?c \<le> rsize q"
+    using RSTAR.IH[OF _ _ _ nf_c] RSTAR.prems by auto
+  show ?case
+    using D1_RSTAR_le_rsize_from_child[of q k] RSTAR.prems child by auto
+next
+  case (RNTIMES q n)
+  then show ?case by simp
+next
+  case (RBACKREF4 q1 q2 q3 q4 cs)
+  then show ?case by simp
+next
+  case (RHALF q cs rep)
+  then show ?case by simp
+next
+  case (RRESIDUE cs rep)
+  then show ?case by simp
+qed
+
+lemma D1_singleton_le_rsize_from_seq_head_suc_boundary_L1_clean:
+  assumes L1:
+    "\<And>rs k. A (RALTS [RALTS rs]) k \<subseteq>
+      (\<Union>q \<in> set rs. A (RALTS [q]) k)"
+    and seq_head:
+    "\<And>h t k. legacy_rrexp h \<Longrightarrow> rntimes_free h \<Longrightarrow>
+      apder_nf h \<Longrightarrow> apder_nf t \<Longrightarrow> apder_nf k \<Longrightarrow>
+      card ((single_root (RSEQ h t) k \<union>
+        single_term h (rsimp4_SEQ_atom t k)) -
+        (B k \<union> B (rsimp4_SEQ_atom t k))) \<le> Suc (rsize h)"
+    and boundary:
+    "\<And>t k. legacy_rrexp t \<Longrightarrow> rntimes_free t \<Longrightarrow>
+      apder_nf t \<Longrightarrow> apder_nf k \<Longrightarrow>
+      card ((B (rsimp4_SEQ_atom t k) - B k) \<union>
+        (single_term t k - B k)) \<le> D1 t k"
+    and clean: "legacy_rrexp q" "rntimes_free q" "apder_nf q" "apder_nf k"
+  shows "D1 q k \<le> rsize q"
+  using clean
+proof (induction q arbitrary: k)
+  case RZERO
+  then show ?case
+    using D1_RZERO_le_rsize[of k] by simp
+next
+  case RONE
+  then show ?case
+    using D1_RONE_le_rsize[of k] by simp
+next
+  case (RCHAR c)
+  then show ?case
+    using D1_RCHAR_le_rsize[of k c] by simp
+next
+  case (RSEQ q1 q2)
+  have sh:
+    "card ((single_root (RSEQ q1 q2) k \<union>
+      single_term q1 (rsimp4_SEQ_atom q2 k)) -
+      (B k \<union> B (rsimp4_SEQ_atom q2 k))) \<le> Suc (rsize q1)"
+    using RSEQ.prems by (intro seq_head) auto
+  have bnd:
+    "card ((B (rsimp4_SEQ_atom q2 k) - B k) \<union>
+      (single_term q2 k - B k)) \<le> D1 q2 k"
+    using RSEQ.prems by (intro boundary) auto
+  have child: "D1 q2 k \<le> rsize q2"
+    using RSEQ.IH(2) RSEQ.prems by auto
+  show ?case
+    by (rule D1_RSEQ_le_rsize_from_head_suc_boundary_child[OF sh bnd child])
 next
   case (RALTS rs)
   have branch: "\<And>q. q \<in> set rs \<Longrightarrow> D1 q k \<le> rsize q"
