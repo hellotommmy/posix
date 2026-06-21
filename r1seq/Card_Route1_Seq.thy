e7a6a32a62def842f0944b8414de7a6ad6eae089
asplos272 <asplos272@gmail.com>
2026-06-22 03:35:54 +0800

    seq: add exact RSTAR RCHAR alt-left char shift

--- r1seq/Card_Route1_Seq.thy
+++ r1seq/Card_Route1_Seq.thy
@@ -2319,6 +2319,20 @@ lemma single_root_RSEQ_RSTAR_root_shift_RCHAR_RALTS_single_RSEQ_RALTS_single_RCH
       rsimp7_SEQ_atom_def rsimpStrong_ALTs_raw_def rsimpStrong_prune_rows_raw_def
       split: if_splits intro!: card_subset_singleton_le_one)
 
+lemma single_root_RSEQ_RSTAR_root_boundary_RCHAR_RALTS_single_RSEQ_RALTS_single_RCHAR:
+  "card ((single_root
+      (RSEQ (RSTAR r) (RALTS [RSEQ (RALTS [RCHAR c]) (RCHAR e)])) (RCHAR a) -
+      B (rsimp4_SEQ_atom (RSTAR r)
+        (rsimp4_SEQ_atom (RALTS [RSEQ (RALTS [RCHAR c]) (RCHAR e)]) (RCHAR a)))) \<union>
+      (B (rsimp4_SEQ_atom (RSTAR r)
+        (rsimp4_SEQ_atom (RALTS [RSEQ (RALTS [RCHAR c]) (RCHAR e)]) (RCHAR a))) -
+        (B (RCHAR a) \<union>
+          B (rsimp4_SEQ_atom (RALTS [RSEQ (RALTS [RCHAR c]) (RCHAR e)]) (RCHAR a))))) \<le> 1"
+  by (cases "rsimpStrong_raw r")
+    (auto simp add: single_root_def B_alt rsimpStrong_dlform_closure_def
+      rsimp7_SEQ_atom_def rsimpStrong_ALTs_raw_def rsimpStrong_prune_rows_raw_def
+      split: if_splits intro!: card_subset_singleton_le_one)
+
 lemma single_root_RSEQ_RSTAR_root_shift_RCHAR_RALTS_single_RSEQ_RALTS_single_RSTAR:
   "card (single_root
       (RSEQ (RSTAR r) (RALTS [RSEQ (RALTS [RCHAR c]) (RSTAR e)])) (RCHAR a) -
