dab5c9f4b4ba6adbe9ac39ad2ea9aa53f86187b9
asplos272 <asplos272@gmail.com>
2026-06-22 23:06:36 +0800

    Cover singleton ok for nonstar prune tails

--- r1cover/Card_Route1_Cover.thy
+++ r1cover/Card_Route1_Cover.thy
@@ -2232,6 +2232,41 @@ proof
     using split later_cover by blast
 qed
 
+lemma singleton_saa_ok_prune_pair_raw_shared_nonstar_if_ok:
+  assumes earlier: "earlier = RSEQ (RALTS lrs) tail"
+    and later: "later = RSEQ (RALTS rrs) tail"
+    and ok: "singleton_saa_ok q later"
+    and rrs_props: "\<forall>r \<in> set rrs.
+      rtail_nf r \<and> nonalt r \<and> r \<noteq> RZERO"
+    and tail_nf: "rtail_nf tail"
+    and tail0: "tail \<noteq> RZERO"
+    and tail1: "tail \<noteq> RONE"
+    and no_star: "\<And>s. tail \<noteq> RSTAR s"
+  shows "singleton_saa_ok q (rsimpStrong_prune_pair_raw earlier later)"
+  unfolding singleton_saa_ok_def
+proof
+  fix k
+  have K_nf: "rtail_nf (rsimpStrong_raw k)"
+    by (rule rtail_nf_rsimpStrong_raw)
+  have split:
+      "row_dlforms
+        (rsimp7_SEQ_atom
+          (rsimpStrong_prune_pair_raw earlier later)
+          (rsimpStrong_raw k)) \<subseteq>
+       row_dlforms (rsimp7_SEQ_atom later (rsimpStrong_raw k))"
+    by (rule row_dlforms_rsimp7_prune_pair_shared_nonstar_tail_subset_later
+        [OF earlier later rrs_props tail_nf tail0 tail1 no_star K_nf])
+  have later_cover:
+      "row_dlforms (rsimp7_SEQ_atom later (rsimpStrong_raw k)) \<subseteq>
+       strong_apder_acc (RALTS [q]) k"
+    using ok unfolding singleton_saa_ok_def by blast
+  show "row_dlforms
+      (rsimp7_SEQ_atom (rsimpStrong_prune_pair_raw earlier later)
+        (rsimpStrong_raw k)) \<subseteq>
+    strong_apder_acc (RALTS [q]) k"
+    using split later_cover by blast
+qed
+
 lemma tagged_Strong_ALTs_rows_singleton_saa_ok:
   assumes qt: "(q, t) \<in> set (tagged_Strong_ALTs_rows [q])"
   shows "singleton_saa_ok q t"
